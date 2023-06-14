90fe8452cf634291a258d0a837bb76a8cec06e5d
Guillaume <guillaume.vige@gmail.com>
2023-06-14 14:43:40 +0200

    ajout d apres openclassroom

--- tuto.c
+++ tuto.c
@@ -192,6 +192,98 @@ void test3()
 }
 
 //**********************************************************************
+//d'apres https://openclassrooms.com/fr/courses/19980-apprenez-a-programmer-en-c/19733-stockez-les-donnees-avec-les-listes-chainees
+typedef struct Element Element;
+struct Element
+{
+  int nombre;
+  Element *suivant;
+};
+
+typedef struct Liste Liste;
+struct Liste
+{
+  Element *premier;
+};
+
+Liste *initialisation()
+{
+  Liste *liste = malloc(sizeof(*liste));
+  Element *element = malloc(sizeof(*element));
+  
+  if (liste == NULL || element == NULL)
+  {
+    exit(EXIT_FAILURE);
+  }
+  
+  element->nombre = 0;
+  element->suivant = NULL;
+  liste->premier = element;
+  
+  return liste;
+}
+
+void insertion(Liste *liste, int nvNombre)
+{
+  /* Création du nouvel élément */
+  Element *nouveau = malloc(sizeof(*nouveau));
+  if (liste == NULL || nouveau == NULL)
+  {
+    exit(EXIT_FAILURE);
+  }
+  nouveau->nombre = nvNombre;
+
+  /* Insertion de l'élément au début de la liste */
+  nouveau->suivant = liste->premier;
+  liste->premier = nouveau;
+}
+
+void suppression(Liste *liste)
+{
+  if (liste == NULL)
+  {
+    exit(EXIT_FAILURE);
+  }
+
+  if (liste->premier != NULL)
+  {
+    Element *aSupprimer = liste->premier;
+    liste->premier = liste->premier->suivant;
+    free(aSupprimer);
+  }
+}
+
+void afficherListe(Liste *liste)
+{
+  if (liste == NULL)
+  {
+    exit(EXIT_FAILURE);
+  }
+
+  Element *actuel = liste->premier;
+
+  while (actuel != NULL)
+  {
+    printf("%d -> ", actuel->nombre);
+    actuel = actuel->suivant;
+  }
+  printf("NULL\n");
+}
+
+void test4()
+{
+  printf("\nTest4 (liste chainee) ---------------\n");
+  Liste *maListe = initialisation();
+
+  insertion(maListe, 4);
+  insertion(maListe, 8);
+  insertion(maListe, 15);
+  suppression(maListe);
+
+  afficherListe(maListe);
+}
+
+//**********************************************************************
 
 int main()
 {
@@ -204,6 +296,7 @@ int main()
   use_memcpy = 1;
   test2();
   test3();
+  test4();
   return 0;
 }
 
